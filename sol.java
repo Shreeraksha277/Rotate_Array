1442dbd8eab3cffc0aaae6efd1a06621abd659d4
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-04-09 17:10:47 +0530

    Fix indexing in array rotation logic

--- sol.java
+++ sol.java
@@ -5,7 +5,7 @@ class Solution {
         k=k % n;
         for(int i=0;i<n;i++)
         {
-            temp[(i+k)%n] = nums[i];
+             temp[(i+k)%n] = nums[i];
         }
         for(int i=0;i<n;i++)
         {
